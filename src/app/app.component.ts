38e715da20d26c750cea4ea7224f9631dea9eef8
Jared Ruiz <jruiz@ceiamerica.com>
2018-08-14 12:52:24 -0400

    adds article picker component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
+
 export class AppComponent {
   title = 'app';
+ 
+  filterList(journal) {
+    console.log('in app component: ' + journal);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ArticlesListComponent } from './articles-list.component';
+import { ArticlePickerComponent } from './article-picker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticlesListComponent
+    ArticlesListComponent,
+    ArticlePickerComponent
   ],
   imports: [
     BrowserModule
